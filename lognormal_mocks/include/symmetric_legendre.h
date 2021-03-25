34e71ea5c5af620c2e6ab9b7ef93c9212a23117e
huffenbe <khuffenberger@fsu.edu>
2021-03-25 12:36:05 -0400

    Renaming the module directory

--- lognormal_mocks/include/symmetric_legendre.h
+++
@@ -1,8 +0,0 @@
-#include <healpix_legendre_c.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_spline.h>
-
-
-void W2Cl(double *theta, double *W, int Ntheta, double *l, double *C, int Nl);
-void Cl2W(double *theta, double *W, int Ntheta, double *l, double *C, int Nl);
-void Cl2W_nospline(double *theta, double *W, int Ntheta, double *C, int Nl); // uses every ell
