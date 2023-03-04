3b0c5c92f800867b9506e4d2395fd2175005b598
huffenbe <khuffenberger@fsu.edu>
2023-03-03 22:21:21 -0500

    Added libsharp as a dependency for recent libhealpix.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ numpy_inc = os.path.join(get_python_lib(plat_specific=1), 'numpy/core/include')
 module2 =  Extension('lognormal_mocks.lognormal_mocks_mod',
                      sources = ['lognormal_mocks/src/mod.c','lognormal_mocks/src/healpix_legendre_c.c','lognormal_mocks/src/lognormal_mocks_stats.c','lognormal_mocks/src/symmetric_legendre.c'],
                      include_dirs = [numpy_inc,'lognormal_mocks/include/'],
-                     libraries=['gsl','gslcblas','healpix','cfitsio','gomp','lapack'],
+                     libraries=['gsl','gslcblas','healpix','cfitsio','gomp','lapack','sharp'],
                      extra_compile_args=['-fPIC','-Wall','-g'])
 
 setup (name = 'lognormal_mocks',
@@ -16,6 +16,5 @@ setup (name = 'lognormal_mocks',
        description = 'lognormal mocks function',
        packages =['lognormal_mocks'],
        ext_modules = [module2]
-       #package_dir = {'lognormal_mocks' : 'code'}
        )
 
